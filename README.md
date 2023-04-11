3335f4a9872689a4d5513697b830179fec15784c
정제우 <wpdn3825@naver.com>
2023-04-11 23:24:07 +0900

    movie 리스트 생성

--- README.md
+++ README.md
@@ -5,3 +5,42 @@
 - 파일 생성
 - json.package 수정
   - package.json.script.(test,eject) 제거
+- props
+
+  ```javascript
+  function test(props) {
+    //{fav} = props;
+    return <h1>{props.fav}</h1>;
+  }
+
+  function eject({ fav }) {
+    return <h1>{fav}</h1>;
+  }
+
+  // 둘중 아무거나 사용해도 된다
+  ```
+
+# 🤦🏻‍♂️ 구조분해할당
+
+# import PropTypes from "prop-types"; 이 뭐야?
+
+- 'PropTypes'는 React 컴포넌트에 전달되는 props의 예상 데이터 유형을 정의하는 방법을 제공하는 React의 'prop-types' 모듈 속성입니다.
+- React에서 구성 요소를 만들 때 객체를 사용하여 구성 요소를 정의할 수 있습니다. PropTypes는 이러한 소품의 유형과 요구 사항을 정의하는 방법입니다.
+- 우리는 prop이 문자열, 배열 또는 객체가 되어야 한다고 정의할 수 있으며 prop이 필수인지 선택 사항인지도 지정할 수 있습니다. 이를 통해 구성 요소가 올바르게 사용되고 수신된 데이터를 처리할 수 있습니다.
+
+```
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function MyComponent(props) {
+  return <div>{props.myProp}</div>;
+}
+
+MyComponent.propTypes = {
+  myProp: PropTypes.string.isRequired
+};
+
+export default MyComponent;
+```
+
+이 예에서는 myProp 소품이 필수 문자열임을 정의합니다. myProp이 제공되지 않거나 문자열이 아닌 다른 것으로 제공되면 콘솔에 경고가 기록됩니다. 이를 통해 개발 프로세스 초기에 버그와 오류를 발견할 수 있습니다.
--- src/App.js
+++ src/App.js
@@ -1,5 +1,53 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import Movie from "./Movie";
+import "./App.css";
+
 function App() {
-  return <div className="App"></div>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts.mx/api/v2/list_movies.json?sort_by=rating"
+    );
+    setMovies(movies);
+    setIsLoading(false);
+    // console.log(movies);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return (
+    <section className="container">
+      {isLoading ? (
+        <div className="loader">
+          <span className="loader__text">Loading...</span>
+        </div>
+      ) : (
+        <div className="movies">
+          {movies.map((movie) => {
+            return (
+              <Movie
+                key={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            );
+          })}
+        </div>
+      )}
+    </section>
+  );
 }
 
 export default App;
